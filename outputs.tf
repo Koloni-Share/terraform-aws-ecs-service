4423a4abc5abb9dc4db359a82f4092a530cff952
Justin Rubek <justin@koloni.me>
2022-04-13 07:33:00 +0000

    Removed ecr output

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "service_name" {
   value = var.service_name
 }
 
-output "ecr_repo_name" {
-  value = aws_ecr_repository.repo.name
-}
-
 output "lb_zone_id" {
   value = module.service.aws_lb_lb_zone_id
 }
